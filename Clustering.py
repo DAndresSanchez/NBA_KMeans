7927255ca48942465e1c1498fe61f3c96bb1110e
David Andres <andressanchezdavid@gmail.com>
2020-05-16 14:13:34 +0100

    PCA and Logistic Regression Classifier (WIP)

--- Clustering.py
+++ Clustering.py
@@ -147,3 +147,26 @@ ax.legend(label)
 ax.grid()
 
 
+#%% WIP PCA and Logistic Regression classifier
+
+https://towardsdatascience.com/principal-component-analysis-for-dimensionality-reduction-115a3d157bad
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.transform(X_test)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+
+# intialize pca and logistic regression model
+pca = PCA(n_components=2)
+lr = LogisticRegression(multi_class='auto', solver='liblinear')
+
+# fit and transform data
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+lr.fit(X_train_pca, y_train)
+
+
+
+
